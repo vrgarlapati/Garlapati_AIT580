f384420c50c28b1cc6cd82498de8164179f274f3
vrgarlapati <98785107+vrgarlapati@users.noreply.github.com>
2022-05-12 00:07:38 -0400

    Update Garlapati_G01354939.R

--- Garlapati_Assignment 15/Garlapati_G01354939.R
+++ Garlapati_Assignment 15/Garlapati_G01354939.R
@@ -15,4 +15,4 @@ head(Nytimes)
 colnames(Nytimes)
 result=Nytimes[,c(8,14,15,16)]
 
-# write.csv(result, "https://github.com/vrgarlapati/Garlapati_AIT580/blob/main/Garlapati_Assignment%2015/res.csv")
+write.csv(result, "https://github.com/vrgarlapati/Garlapati_AIT580/blob/main/Garlapati_Assignment%2015/res.csv")
