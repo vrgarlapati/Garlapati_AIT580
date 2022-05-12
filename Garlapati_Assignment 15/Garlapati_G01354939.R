4a68c2c6017d77fe86f71dc2391ca53152ba87cc
vrgarlapati <vgarlapa@gmu.edu>
2022-05-11 23:52:35 -0400

    s

--- Garlapati_Assignment 15/Garlapati_G01354939.R
+++ Garlapati_Assignment 15/Garlapati_G01354939.R
@@ -15,4 +15,4 @@ head(Nytimes)
 colnames(Nytimes)
 result=Nytimes[,c(8,14,15,16)]
 
-write.csv(result, "D:/GMU Student/Github/Garlapati_AIT580/Garlapati_Assignment 15/res.csv")
+write.csv(result, "https://github.com/vrgarlapati/Garlapati_AIT580/blob/main/Garlapati_Assignment%2015/res.csv")
